8e143319a4ab13ce6627bf4dc6e8dd774c4878c1
Ioannis Horgos <jhor@acta.com.gr>
2023-12-30 21:51:38 +0200

    added swagger documentation

--- Program.cs
+++ Program.cs
@@ -1,5 +1,19 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "STUDYstics API", Description = "CRUD operation for STUDYstics DB", Version = "v1" });
+    });
+
+
 var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/schema/swagger/swagger.json", "STUDYstics API docs");
+});
 
 app.MapGet("/", () => "Hello World!");
 
